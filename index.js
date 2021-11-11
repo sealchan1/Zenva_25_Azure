165c1457e903a34ef97430c00cf774ca1738b1af
sealchan1 <sealchan@yahoo.com>
2021-11-11 06:45:21 -0800

    random changes

--- index.js
+++ index.js
@@ -26,9 +26,8 @@ app.set('view engine', 'hjs');
 
 // Listen
 
-//let port = 3000; // 3000
 let port = process.env.PORT || 3000;
 app.listen(port);
-console.log(`Running and listening on port ${port}`);
+console.log(`Running, and listening on port ${port}`);
 console.log('Hello World');
 console.log(process.env.DB_HOST);
